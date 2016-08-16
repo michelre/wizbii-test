2a729ad5cbad8b1c7d28c213fe1c4177018b5ad3
Rémi Michel <remi.michel@kaizen-solutions.net>
2016-08-16 11:15:54 +0200

    fix bug environment PORT

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const request = require('request');
 const app = express();
 
-const PORT = process.env.port || 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.static(`${__dirname}`));
 
