63f2672a6b45e4745666d715382e338cd805debf
Rémi Michel <remi.michel@kaizen-solutions.net>
2016-08-16 14:50:06 +0200

    test webpack

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -7,7 +7,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, "dist"),
-    publicPath: `http://localhost:${process.env.PORT}/dist/`,
+    publicPath: `https://wizbii-test-michelre.herokuapp.com:${process.env.PORT}/dist/`,
     filename: 'bundle.js',
   },
   module: {
